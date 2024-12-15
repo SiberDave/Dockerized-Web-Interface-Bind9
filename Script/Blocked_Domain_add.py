dd6be7089cdabba9f12d1655c93b00848c52cb66
SiberDave <dave.cloud.cyber@gmail.com>
2024-12-16 02:52:59 +0700

    Fix on function add domain block

--- Script/Blocked_Domain_add.py
+++ Script/Blocked_Domain_add.py
@@ -4,11 +4,12 @@ import sys
 from helper import search_for_string, add_block
 
 domain = sys.argv[1]
-path = r"./try_bind_conf"
+category = sys.argv[2]
+path = r"/etc/bind/db.blocked.rpz"
 
 status = search_for_string(domain,path)
 if status == False:
-    add_block(domain,path,"Uncategorized")
+    add_block(domain,path,category)
     print("Domain " + domain + " is successfully blocked!")
 else:
     print("Domain is already blocked!")
--- Script/run_service.sh
+++ Script/run_service.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 systemctl start named node_api 
-/home/webScript/Blocked_Domain_add.sh db.ads.rpz doubleclick.net A
-/home/webScript/Blocked_Domain_add.sh db.ads.rpz doubleclick.net AAAA
+/home/webScript/Blocked_Domain_add.py doubleclick.net "google ads"
